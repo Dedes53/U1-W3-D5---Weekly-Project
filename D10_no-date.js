07b8e10f535d45121b9f0e76344948ecbbefb0da
Dedes73 <federico.lepore73@gmail.com>
2025-12-12 13:28:30 +0100

    es 16

--- D10_no-date.js
+++ D10_no-date.js
@@ -369,6 +369,15 @@ console.log(onlyInLastMillennium(movies));
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
 */
+function sumAllTheYears(array) {
+  let sum = 0;
+  array.forEach((movie) => {
+    sum += parseInt(movie.Year);
+  });
+
+  return sum;
+}
+console.log(sumAllTheYears(movies));
 
 /* ESERCIZIO 17
   Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
